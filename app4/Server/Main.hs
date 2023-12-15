07415c31acac399bca1b43f1089f7576c67eaf3f
Gintare Keraite <gintare.keraite18@gmail.com>
2023-12-15 17:14:22 +0200

    loading from memory + saving every 1s

--- app4/Server/Main.hs
+++ app4/Server/Main.hs
@@ -1,52 +1,101 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
+{-# HLINT ignore "Use newtype instead of data" #-}
 module Main (main) where
 
 import Web.Spock
 import Web.Spock.Config
+import Control.Monad (forever)
 import Data.Text.Encoding (decodeUtf8)
-import Shared (webServerPort, SqlRequest (..), SqlErrorResponse (..), SqlSuccessResponse (..))
+import Shared (webServerPort, SqlRequest(..), SqlErrorResponse(..), SqlSuccessResponse(..))
 import Network.HTTP.Types.Status
-import DataFrame (DataFrame (..), Column(..))
-import InMemoryTables (tableEmployees)
+import DataFrame (DataFrame(..), Column(..))
+import InMemoryTables (TableName)
 import Data.Time (UTCTime, getCurrentTime)
 import Lib3 qualified
-import Control.Monad.IO.Class (MonadIO (liftIO))
-import Control.Monad.Free (Free (..))
+import Data.Functor
+import Control.Monad.IO.Class (MonadIO(liftIO))
+import Control.Monad.Free (Free(..))
 import System.Directory (listDirectory, doesFileExist)
 import System.FilePath (dropExtension, pathSeparator, takeExtension)
+import Control.Concurrent.STM.TVar (TVar, newTVarIO, readTVarIO, modifyTVar', readTVar, newTVar)
+import Control.Concurrent.STM (atomically)
+import Control.Concurrent (threadDelay, forkIO)
+import Data.Either (partitionEithers)
+import Data.List (intercalate, find)
 
-type ApiAction a = SpockAction () () () a
+type FileContent = String
+
+type ConcurrentTable = TVar (TableName, DataFrame)
+type ConcurrentDb = TVar [ConcurrentTable]
+data AppState = AppState { db :: ConcurrentDb }
+type ApiAction a = SpockAction () () AppState a
+
+dbDirectory :: String
+dbDirectory = "db"
+
+dbFormat :: String
+dbFormat = ".yaml"
 
 main :: IO ()
 main = do
-  cfg <- defaultSpockCfg () PCNoDatabase ()
+  dbContents <- liftIO loadDb
+  db <- newTVarIO =<< mapM newTVarIO dbContents
+  cfg <- defaultSpockCfg () PCNoDatabase (AppState db)
+  forkIO $ syncDb db
   runSpock webServerPort (spock cfg app)
 
-app :: SpockM () () () ()
-app = do
-  post root $ do
-    mreq <- jsonBody :: ApiAction (Maybe SqlRequest)
-    case mreq of
-      Nothing -> do
-        setStatus badRequest400
-        json SqlErrorResponse { errorMessage = "Request body format is incorrect." }
-      Just req -> do
-        result <- liftIO $ runExecuteIO $ Lib3.executeSql $ query req
-        case result of
-          Left err -> do
-            setStatus badRequest400
-            json SqlErrorResponse { errorMessage = err }
-          Right df -> do
-            json SqlSuccessResponse { dataFrame = df }
+app :: SpockM () () AppState ()
+app = post root $ do
+  appState <- getState
+  mreq <- jsonBody' :: ApiAction SqlRequest
+  result <- liftIO $ runExecuteIO (db appState) (Lib3.executeSql (query mreq))
+  case result of
+    Left err -> do
+      setStatus badRequest400
+      json SqlErrorResponse { errorMessage = err }
+    Right df -> json SqlSuccessResponse { dataFrame = df }
 
-runExecuteIO :: Lib3.Execution r -> IO r
-runExecuteIO (Pure r) = return r
-runExecuteIO (Free step) = do
-    next <- runStep step
-    runExecuteIO next
+getDbTables :: IO [String]
+getDbTables = do
+  filesInDir <- listDirectory dbDirectory
+  return [ dropExtension fileName | fileName <- filesInDir, takeExtension fileName == dbFormat ]
+
+getTableFilePath :: String -> String
+getTableFilePath tableName = dbDirectory ++ [pathSeparator] ++ tableName ++ dbFormat
+
+
+loadDb :: IO [(TableName, DataFrame)]
+loadDb = do
+  tableNames <- getDbTables
+  contents <- mapM (readFile . getTableFilePath) tableNames
+  case Lib3.parseTables contents of
+    Left err -> do
+      putStrLn $ "Error loading tables: " ++ err
+      return []
+    Right tables -> return tables
+
+syncDb :: ConcurrentDb -> IO ()
+syncDb dbVar = forever $ do
+  threadDelay $ 1 * 1000 * 1000 -- 1 second
+  tables <- atomically $ readTVar dbVar
+  mapM_ syncTable tables
+
+syncTable :: ConcurrentTable -> IO ()
+syncTable tableVar = do
+  (tableName, tableData) <- atomically $ readTVar tableVar
+  let serializedTable = Lib3.dataFrameToSerializedTable (tableName, tableData)
+  let serializedYAML = Lib3.serializeTableToYAML serializedTable
+  writeFile (getTableFilePath tableName) serializedYAML
+
+runExecuteIO :: ConcurrentDb -> Lib3.Execution r -> IO r
+runExecuteIO dbRef (Pure r) = return r
+runExecuteIO dbRef (Free step) = do
+  next <- runStep step
+  runExecuteIO dbRef next
   where
     runStep :: Lib3.ExecutionAlgebra a -> IO a
-    runStep (Lib3.GetTime next) = getCurrentTime >>= return . next
+    runStep (Lib3.GetTime next) = getCurrentTime <&> next
     runStep (Lib3.LoadFiles tableNames next) = do
       tablesExist <- filesExist (map getTableFilePath tableNames)
       if tablesExist
@@ -55,24 +104,43 @@ runExecuteIO (Free step) = do
           return $ next $ Right fileContents
         else
           return $ next $ Left "One or more provided tables does not exist"
-
     runStep (Lib3.UpdateTable (tableName, df) next) = do
-        let serializedTable = Lib3.dataFrameToSerializedTable (tableName, df)
-        let yamlContent = Lib3.serializeTableToYAML serializedTable
-        writeFile (getTableFilePath tableName) yamlContent
-        return next
+      tableVar <- findOrCreateTable dbRef tableName
+      atomically $ modifyTVar' tableVar (\(_, _) -> (tableName, df))
+      return next
 
-columnName :: DataFrame.Column -> String
-columnName (DataFrame.Column name _) = name 
+findOrCreateTable :: ConcurrentDb -> TableName -> IO ConcurrentTable
+findOrCreateTable dbRef tableName = do
+  tables <- atomically $ readTVar dbRef
+  foundTableVar <- findTableByName tables tableName
+  case foundTableVar of
+    Just tableVar -> return tableVar
+    Nothing -> atomically $ do
+      newTableVar <- newTVar (tableName, emptyDataFrame)
+      modifyTVar' dbRef (newTableVar :)
+      return newTableVar
 
-getTableFilePath :: String -> String
-getTableFilePath tableName = "db/" ++ tableName ++ ".yaml"
+findTableByName :: [ConcurrentTable] -> TableName -> IO (Maybe ConcurrentTable)
+findTableByName tables tableName = do
+  withNames <- mapM (\tblVar -> atomically $ do
+                                 (name, _) <- readTVar tblVar
+                                 return (name, tblVar)) tables
+  return $ lookup tableName withNames
+
+findTable :: ConcurrentDb -> TableName -> IO (Maybe ConcurrentTable)
+findTable dbRef tableName = do
+  tables <- atomically $ readTVar dbRef
+  findTableByName tables tableName
+
+
+emptyDataFrame :: DataFrame
+emptyDataFrame = DataFrame [] []  -- idk ar cia teisingai?
 
 filesExist :: [String] -> IO Bool
-filesExist [tableName] = doesFileExist tableName
-filesExist (tableName : xs) = do
-  tableExists <- doesFileExist tableName
-  rest <- filesExist xs
-  return $ tableExists && rest
-filesExist _ = return True
-  
+filesExist files = allM doesFileExist files
+
+allM :: Monad m => (a -> m Bool) -> [a] -> m Bool
+allM _ [] = return True
+allM p (x:xs) = do
+  result <- p x
+  if result then allM p xs else return False
